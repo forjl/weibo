bba012b623e634245c8582155ec489c8488204d8
forjl <748409412@qq.com>
2019-03-14 07:24:55 +0000

    homepage

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -23,10 +23,7 @@
   @else
 
   <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
-    </p>
+    <h1>Hello</h1>
     <p>
       一切，将从这里开始。
     </p>
