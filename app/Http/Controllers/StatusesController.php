472048dcad7bda88184e8232c554a3e210fa7b97
forjl <748409412@qq.com>
2019-03-14 02:50:07 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -28,6 +29,9 @@ class StatusesController extends Controller
 
     public function destroy(Status $status)
     {
-
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
     }
 }
